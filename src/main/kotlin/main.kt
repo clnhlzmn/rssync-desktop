b410b26dcdbdac40267e9303bdf76d8ef89e9c97
Colin Holzman <colin.holzman@protonmail.com>
2019-05-01 13:07:58 -0400

    initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,19 @@
+
+import java.awt.Toolkit
+import java.awt.datatransfer.DataFlavor
+import java.awt.datatransfer.StringSelection
+
+fun main() {
+    val t = Toolkit.getDefaultToolkit().systemClipboard.getContents(null)
+    try {
+        if (t != null && t.isDataFlavorSupported(DataFlavor.stringFlavor)) {
+            var text = t.getTransferData(DataFlavor.stringFlavor) as String
+            text = text.toUpperCase();
+            val ss = StringSelection(text);
+            Toolkit.getDefaultToolkit().systemClipboard.setContents(ss, null);
+        }
+    } catch (e: Exception) {
+        print("Error: $e")
+    }
+}
+
