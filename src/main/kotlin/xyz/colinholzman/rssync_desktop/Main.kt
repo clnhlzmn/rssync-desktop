da09be7f62fb1958bb238fcd6d846b7ad2ef502c
Colin Holzman <colin.holzman@protonmail.com>
2019-05-02 12:59:42 -0400

    add some todos

--- src/main/kotlin/xyz/colinholzman/rssync_desktop/Main.kt
+++ src/main/kotlin/xyz/colinholzman/rssync_desktop/Main.kt
@@ -3,32 +3,41 @@ package xyz.colinholzman.rssync_desktop
 import java.awt.Toolkit
 import java.awt.datatransfer.DataFlavor
 import java.awt.datatransfer.StringSelection
-import java.util.concurrent.Executors
-import java.util.concurrent.TimeUnit
 import xyz.colinholzman.remotestorage_kotlin.RemoteStorage
 
 fun main(args: Array<String>) {
 
-    val rs = RemoteStorage("", "")
+    //TODO: get user address and token from args
+    val userAddress = ""
+    val token = ""
 
-    val task = Runnable { print("task") }
-    val executorService = Executors.newSingleThreadScheduledExecutor();
-    executorService.scheduleAtFixedRate(task, 0, 10, TimeUnit.SECONDS);
+    //TODO: get href from user address using Discovery
+    val href = userAddress
+    val rs = RemoteStorage(href, token)
 
-    val t = Toolkit.getDefaultToolkit().systemClipboard.getContents(null)
-    try {
-        if (t != null && t.isDataFlavorSupported(DataFlavor.stringFlavor)) {
-            var text = t.getTransferData(DataFlavor.stringFlavor) as String
-            text = text.toUpperCase();
-            val ss = StringSelection(text);
-            Toolkit.getDefaultToolkit().systemClipboard.setContents(ss, null);
+    var lastRemote: String? = null
+    var lastLocal: String? = null
+
+    val cb = Toolkit.getDefaultToolkit().systemClipboard
+    while (true) {
+        try {
+            //check for local changes
+            val t = cb.getContents(null)
+            if (t == null && lastLocal != null) {
+                //TODO: delete remote
+            } else if (t != null && t.isDataFlavorSupported(DataFlavor.stringFlavor)) {
+                var text = t.getTransferData(DataFlavor.stringFlavor) as String
+                text = text.toUpperCase();
+                val ss = StringSelection(text);
+                Toolkit.getDefaultToolkit().systemClipboard.setContents(ss, null);
+            }
+
+            //check for remote changes
+
+        } catch (e: Exception) {
+            print("Error: $e")
         }
-    } catch (e: Exception) {
-        print("Error: $e")
     }
 
-    while (!executorService.isShutdown) {
-        continue
-    }
 }
 
