e43f408ab64e64b749c82aa2e6c44a19d523c0dc
Colin Holzman <colin.holzman@protonmail.com>
2019-05-20 12:42:06 -0400

    add icon, start to make system tray item

--- src/main/kotlin/xyz/colinholzman/rssync_desktop/Main.kt
+++ src/main/kotlin/xyz/colinholzman/rssync_desktop/Main.kt
@@ -1,21 +1,70 @@
 package xyz.colinholzman.rssync_desktop
 
-import xyz.colinholzman.remotestorage_kotlin.*
+import net.sf.image4j.codec.ico.ICODecoder
 import java.awt.*
-import java.awt.event.MouseAdapter
-import java.awt.event.MouseEvent
-import java.util.*
 import javax.swing.SwingUtilities
+import java.awt.AWTException
+import java.awt.CheckboxMenuItem
+import java.awt.SystemTray
+import java.awt.TrayIcon
+import java.io.File
 
 
 class Main {
 
     companion object {
 
+        fun addSystemTrayIcon() {
+            if (!SystemTray.isSupported()) {
+                println("SystemTray is not supported")
+                return
+            }
+            val popup = PopupMenu()
+            val icon = ICODecoder.read(File(Main::class.java.getResource("/SyncArrow.ico").toURI())).firstOrNull()
+            val trayIcon = TrayIcon(icon, "tray icon")
+            val tray = SystemTray.getSystemTray()
+
+            // Create a pop-up menu components
+            val aboutItem = MenuItem("About")
+            val cb1 = CheckboxMenuItem("Set auto size")
+            val cb2 = CheckboxMenuItem("Set tooltip")
+            val displayMenu = Menu("Display")
+            val errorItem = MenuItem("Error")
+            val warningItem = MenuItem("Warning")
+            val infoItem = MenuItem("Info")
+            val noneItem = MenuItem("None")
+            val exitItem = MenuItem("Exit")
+
+            //Add components to pop-up menu
+            popup.add(aboutItem)
+            popup.addSeparator()
+            popup.add(cb1)
+            popup.add(cb2)
+            popup.addSeparator()
+            popup.add(displayMenu)
+            displayMenu.add(errorItem)
+            displayMenu.add(warningItem)
+            displayMenu.add(infoItem)
+            displayMenu.add(noneItem)
+            popup.add(exitItem)
+
+            trayIcon.popupMenu = popup
+
+            try {
+                tray.add(trayIcon)
+            } catch (e: AWTException) {
+                println("TrayIcon could not be added.")
+            }
+
+        }
+
+
         val rs = RSSync()
 
         @JvmStatic
         fun main(args: Array<String>) {
+
+            addSystemTrayIcon()
 
             SwingUtilities.invokeLater {
                 val settings = SettingsDialog()
